12c9773adfc4bbed480d94cde7e0bbe6dbd6e007
horimusang <124416318+diacond@users.noreply.github.com>
2025-06-06 16:52:37 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for(int i=0; i<s.length(); i++){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            cnt++;
+    }
+    cout << cnt;
+}
