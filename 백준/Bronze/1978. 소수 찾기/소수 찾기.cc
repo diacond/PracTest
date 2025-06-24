b8baa02b791fd78dce0c14ed23d2da2c7d0e5acc
horimusang <124416318+diacond@users.noreply.github.com>
2025-06-25 00:06:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    int sosu = 0;
+    int cnt1 = 0;
+    
+    cin >> N;
+
+    for(int i=0; i<N; i++){
+        int n;
+        cin>>n;
+ 
+        int cnt=0;
+ 
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                cnt++;
+            }
+        }
+        if(cnt==2){
+            cnt1++;
+        }
+    }
+    cout<<cnt1;
+    return 0;
+}
