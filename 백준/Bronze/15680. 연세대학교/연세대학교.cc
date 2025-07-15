a122256378b660436ddfbbb74542b76924dadce4
horimusang <124416318+diacond@users.noreply.github.com>
2025-07-15 23:40:34 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    if(N == 1)
+       cout<<"Leading the Way to the Future"<<endl;
+    else
+        cout<<"YONSEI"<<endl;
+    return 0;
+}
