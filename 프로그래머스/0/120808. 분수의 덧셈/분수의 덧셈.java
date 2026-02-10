b736fce4ca5415c1e93caa7c377fa41230c66225
horimusang <124416318+diacond@users.noreply.github.com>
2026-02-10 15:06:41 +0900

    [level 0] Title: 분수의 덧셈, Time: 4.73 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -2,17 +2,16 @@ class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int son = numer1*denom2 + numer2*denom1;
         int mom = denom1*denom2;
-        int max_divide = 1;
-        int min = (son>mom)?mom:son;
+        int sml = (son > mom)?mom:son;
+        int m_divide = 1;
         
-        for(int i=1; i<= min; i++){
-            if(son % i == 0 && mom % i ==0) max_divide = i;
+        for(int i = 1; i <= sml; i++){
+            if(son % i == 0 && mom % i == 0) m_divide = i;
         }
         
         int[] answer = new int[2];
-        answer[0] = son/max_divide;
-        answer[1] = mom/max_divide;
-        
+        answer[0] = son / m_divide;
+        answer[1] = mom / m_divide;
         return answer;
     }
 }
