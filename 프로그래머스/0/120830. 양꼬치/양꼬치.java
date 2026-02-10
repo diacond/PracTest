9ed0e2db612f20175b2f5822dde21d874ebc3549
horimusang <124416318+diacond@users.noreply.github.com>
2026-02-10 16:41:50 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        int free_drink = 0;
-        if(n >= 10){
-            free_drink = n/10;
-        }
-        answer = 12000 * n + 2000 * (k - free_drink);
+        answer = 12000 * n + 2000 * (k - n/10);
         return answer;
     }
 }
