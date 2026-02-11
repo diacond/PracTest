009ed35b24d7b443cb50b9748f77fe31f54c52d3
horimusang <124416318+diacond@users.noreply.github.com>
2026-02-11 20:34:52 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.50 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        int n = numbers.length;
+        answer = numbers[n-1] * numbers[n-2];
+        return answer;
+    }
+}
